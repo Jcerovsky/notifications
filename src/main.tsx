0ff83d4248605bfe18f9746845d85fcde952bae6
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-08-21 13:37:02 +0930

    wrap components in context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Notifications from "./Notifications";
+import { NotificationProvider } from "./Context";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Notifications />
+    <NotificationProvider>
+      <Notifications />
+    </NotificationProvider>
   </React.StrictMode>,
 );
